6af3623742c03f62220a0bedcc2974a982b280f1
Isaac Marco Blancas <marcoblancas@gmail.com>
2020-11-16 12:11:24 +0100

    MDL-63296 profile: display phone1, phone2 and other fields in profile
    Sets phone1,phone2,institution,department,idnumber,address as
    default selected hiddenuserfields in the "User policies" config and
    modify de profile.php in order to control its visibility with this
    config instead to use showuseridentity which should just control
    visibility fields in user lists and not the profile. Admins are
    required to unselect this fields in order to display them in profile.
    
    Includes a db patch in lib/db/upgrade.php in order to add this fields
    in the hiddenuserfields config in upgraded installations to ensure
    this fields stay hidden until an administrator unselect them.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2998,5 +2998,18 @@ function xmldb_main_upgrade($oldversion) {
     // Automatically generated Moodle v3.10.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2020111500.01) {
+
+        // Add phone1,phone2,institution,department,idnumber,address as hiddenuserfields.
+        if (empty($CFG->hiddenuserfields)) {
+            set_config('hiddenuserfields', 'phone1,phone2,institution,department,idnumber,address');
+        } else {
+            set_config('hiddenuserfields', $CFG->hiddenuserfields . ',phone1,phone2,institution,department,idnumber,address');
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2020111500.01);
+    }
+
     return true;
 }
